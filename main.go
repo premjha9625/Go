1a23c65840ea8b4c8b4b838491d003f26d22aedd
premjha <jhasaroj425@gmail.com>
2024-03-13 23:05:00 +0530

    first commit

--- main.go
+++ main.go
@@ -1,3 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
 func main() {
-	println("this is go")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+	http.ListenAndServe(":3000", r)
 }
